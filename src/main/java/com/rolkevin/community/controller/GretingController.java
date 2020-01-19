6934800b8ced399887cc8dccd0ffe445bfc72a87
RolkevinXQ <rolkevin@foxmail.com>
2020-01-19 18:02:19 +0800

    Init Community Project

---
+++ src/main/java/com/rolkevin/community/controller/GretingController.java
@@ -0,0 +1,17 @@
+package com.rolkevin.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/basic")
+public class GretingController {
+    @GetMapping("/")
+    public String getGreting(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "grett";
+    }
+}
